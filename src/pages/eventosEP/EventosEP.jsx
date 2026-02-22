dce1ef882064b8febc42fb88b47fd445f5ed8fae
Damiao Netto <143633691+DamiaoNetto@users.noreply.github.com>
2026-02-22 12:31:32 -0300

    fix: corrigindo case sensitive do InfoCard no import

--- src/pages/eventosEP/EventosEP.jsx
+++ src/pages/eventosEP/EventosEP.jsx
@@ -1,49 +1,46 @@
-import React from 'react'
-import InfoCard from '../../components/infoCards/infoCard'
-import imgJovens from '../../assets/images/empoderando-jovens-futuro.png'
-import carreira from '../../assets/images/carreira-primeiro-emprego.png'
-import tecnologia from '../../assets/images/tecnologia-que-transforma.png'
+import React from "react";
+import InfoCard from "../../components/infoCards/InfoCards";
+import imgJovens from "../../assets/images/empoderando-jovens-futuro.png";
+import carreira from "../../assets/images/carreira-primeiro-emprego.png";
+import tecnologia from "../../assets/images/tecnologia-que-transforma.png";
 
-import s from "../Style.module.scss"
-
+import s from "../Style.module.scss";
 
 export default function EventosEP() {
   return (
     <main className={s.main}>
-
-        <h1>Eventos & Palestras</h1>
+      <h1>Eventos & Palestras</h1>
       <section>
         <article>
           <InfoCard
-           img = {imgJovens}
-          alt = "Imagem de uma pessoa carregando caixa com alimentos"
-          subtitulo = "Empoderando Jovens para o Futuro"
-          paragrafo = "Atividade: Palestra motivacional sobre liderança jovem e transformação social. Impacto: Inspirar adolescentes a se tornarem agentes de mudança em suas comunidades." 
-          textoBotao = "Quero participar"
+            img={imgJovens}
+            alt="Imagem de uma pessoa carregando caixa com alimentos"
+            subtitulo="Empoderando Jovens para o Futuro"
+            paragrafo="Atividade: Palestra motivacional sobre liderança jovem e transformação social. Impacto: Inspirar adolescentes a se tornarem agentes de mudança em suas comunidades."
+            textoBotao="Quero participar"
           />
         </article>
 
         <article>
           <InfoCard
-          img = {tecnologia}
-          alt = "Imagem de uma "
-          subtitulo = "Tecnologia que Transforma"
-          paragrafo = "Atividade: Workshop de introdução à programação e inovação digital.Impacto: Preparar jovens para o mercado de trabalho através da tecnologia." 
-          textoBotao = "Quero participar"
-          
+            img={tecnologia}
+            alt="Imagem de uma "
+            subtitulo="Tecnologia que Transforma"
+            paragrafo="Atividade: Workshop de introdução à programação e inovação digital.Impacto: Preparar jovens para o mercado de trabalho através da tecnologia."
+            textoBotao="Quero participar"
           />
         </article>
 
         <article>
-        <InfoCard
-          img = {carreira}
-          alt = "Imagem de uma "
-          subtitulo = "Carreira e Primeiro Emprego"
-          paragrafo = "Atividade: Palestra com profissionais de RH sobre como preparar currículo, entrevistas e postura no mercado.Impacto: Ajudar jovens a conquistar oportunidades de trabalho." 
-          textoBotao = "Quero participar"/>
+          <InfoCard
+            img={carreira}
+            alt="Imagem de uma "
+            subtitulo="Carreira e Primeiro Emprego"
+            paragrafo="Atividade: Palestra com profissionais de RH sobre como preparar currículo, entrevistas e postura no mercado.Impacto: Ajudar jovens a conquistar oportunidades de trabalho."
+            textoBotao="Quero participar"
+          />
         </article>
-
       </section>
     </main>
-  )
+  );
 }
--- src/pages/mentoria/Mentoria.jsx
+++ src/pages/mentoria/Mentoria.jsx
@@ -1,49 +1,46 @@
-import React from 'react'
-import InfoCard from '../../components/infoCards/infoCard'
-import imgMentoria from '../../assets/images/mentoria-carreira-emprego.png'
-import imgExperiencia from '../../assets/images/compartilhe-experiencia.png'
-import imgAcompanhamento from '../../assets/images/acompanhamento.png'
+import React from "react";
+import InfoCard from "../../components/infoCards/InfoCards";
+import imgMentoria from "../../assets/images/mentoria-carreira-emprego.png";
+import imgExperiencia from "../../assets/images/compartilhe-experiencia.png";
+import imgAcompanhamento from "../../assets/images/acompanhamento.png";
 
-import s from "../Style.module.scss"
-
+import s from "../Style.module.scss";
 
 export default function Mentoria() {
   return (
     <main className={s.main}>
-
-        <h1>Mentoria</h1>
+      <h1>Mentoria</h1>
       <section>
         <article>
           <InfoCard
-           img = {imgMentoria}
-          alt = "Imagem de uma pessoa carregando caixa com alimentos"
-          subtitulo = "Mentoria de Carreira e Emprego"
-          paragrafo = "Orientação sobre currículo, entrevistas e primeiros passos no mercado de trabalho." 
-          textoBotao = "Quero participar"
+            img={imgMentoria}
+            alt="Imagem de uma pessoa carregando caixa com alimentos"
+            subtitulo="Mentoria de Carreira e Emprego"
+            paragrafo="Orientação sobre currículo, entrevistas e primeiros passos no mercado de trabalho."
+            textoBotao="Quero participar"
           />
         </article>
 
         <article>
           <InfoCard
-          img = {imgExperiencia}
-          alt = "Imagem de uma "
-          subtitulo = "Compartilhe Experiência"
-          paragrafo = "Oriente jovens e profissionais iniciantes em sua área." 
-          textoBotao = "Quero participar"
-          
+            img={imgExperiencia}
+            alt="Imagem de uma "
+            subtitulo="Compartilhe Experiência"
+            paragrafo="Oriente jovens e profissionais iniciantes em sua área."
+            textoBotao="Quero participar"
           />
         </article>
 
         <article>
-        <InfoCard
-          img = {imgAcompanhamento}
-          alt = "Imagem de uma "
-          subtitulo = "Acompanhamento"
-          paragrafo = "Participe como guia em jornadas de aprendizado e desenvolvimento." 
-          textoBotao = "Quero participar"/>
+          <InfoCard
+            img={imgAcompanhamento}
+            alt="Imagem de uma "
+            subtitulo="Acompanhamento"
+            paragrafo="Participe como guia em jornadas de aprendizado e desenvolvimento."
+            textoBotao="Quero participar"
+          />
         </article>
-
       </section>
     </main>
-  )
+  );
 }
